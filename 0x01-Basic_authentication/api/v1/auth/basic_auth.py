03f726d636fc1c8cb998183d24593e6b31b5441a
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-02 12:53:06 -0700

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,43 +1,27 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API.
+"""
+import re
 import base64
-from typing import List, TypeVar
+import binascii
+from typing import Tuple, TypeVar
 
-from api.v1.auth.auth import Auth
-
-T = TypeVar('T')
+from .auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
-    """Basic authentication class"""
-
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Extracts the Base64 part of the Authorization header for Basic Authentication"""
-        if authorization_header is None or not isinstance(authorization_header, str):
-            return None
-        if not authorization_header.startswith('Basic '):
-            return None
-        return authorization_header.split(' ')[1]
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if authentication is required for the given path"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        for excluded_path in excluded_paths:
-            if path.startswith(excluded_path):
-                return False
-        return True
-
-    def authorization_header(self, request) -> str:
-        """Extracts the authorization header from the request"""
-        return request.headers.get('Authorization', None)
-
-    def current_user(self, request) -> TypeVar('T'):
-        """Returns the current user"""
+    """Basic authentication class.
+    """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
         return None
-
-    def authenticate_user(self, username: str, password: str) -> bool:
-        """Authenticates the user"""
-        return False
-
-    def authenticate_token(self, token: str) -> bool:
-        """Authenticates the token"""
-        return False
