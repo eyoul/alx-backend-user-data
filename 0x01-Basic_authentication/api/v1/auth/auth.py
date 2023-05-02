0716043dde1d00096a2dd2ed4dcb34e0e5f8d2cc
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-02 05:18:59 -0700

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python3
 """API authentication.
 """
-import re
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
-    """Authentication class.
-    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Returns False for now.
         """
-        if path is not None and excluded_paths is not None:
-            for excluded_paths in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if excluded_paths[-1] == '*':
-                    pattern = '{}.*'.format(excluded_paths[0:-1])
-                elif excluded_paths[-1] == '/':
-                    pattern = '{}/*'.format(excluded_paths[0:-1])
-                else:
-                    pattern = '{}/*'.format(excluded_paths)
-                if re.match(pattern, path):
-                    return False
+        return False
 
     def authorization_header(self, request=None) -> str:
         """
         Returns None for now.
         """
-        if request is not None:
-            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
