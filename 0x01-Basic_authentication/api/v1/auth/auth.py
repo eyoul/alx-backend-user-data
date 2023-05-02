3070442c9751b3f65df6df5e0cb396c9144cd371
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-02 10:04:27 -0700

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,11 @@ class Auth:
         """
         if request is None:
             return None
-        return request.headers.get('Authorization', None)
+        
+        auth_header = request.headers.get('Authorization')
+        if auth_header is None:
+            return None
+        return auth_header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
