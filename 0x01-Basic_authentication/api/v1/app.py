8444604f5245e17450ebb544a2a87cd2b30be8ee
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-02 10:53:06 -0700

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
-
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
