95032a2c2416453925cb758a6cb18827b4bb1f5d
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-02 09:03:53 -0700

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
+auth_type = getenv('AUTH_TYPE')
 if auth_type == 'auth':
     auth = Auth()
 else:
