5f544c6f76f395aec40487e37c334bc793671eee
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 16:02:57 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -10,23 +10,23 @@ from models.user import User
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    """POST /api/v1/auth_session/login
+    """ POST /auth_session/login
+    Return:
+      - User object JSON represented
+      - Sets a cookie for the user
     """
     email = request.form.get('email')
     password = request.form.get('password')
-
     if email is None or email == '':
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if password is None or password == '':
-        return jsonify({ "error": "password missing" }), 400
-
-    user_list = User.search({'email': email})
-    if len(user_list):
-        return jsonify({ "error": "no user found for this email" }), 404
-
-    user = user_list[0]
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({'email': email})
+    if len(user) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     cookie_name = getenv('SESSION_NAME')
