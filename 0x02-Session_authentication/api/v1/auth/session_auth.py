4c17a5ad138bc5d52169d8934e62d52909368c84
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 10:54:19 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,12 +3,13 @@
 """
 import os
 import uuid
+from models.user import User
 from typing import Tuple, TypeVar
 from .auth import Auth
 
 
 class SessionAuth(Auth):
-    """Creating a class attribute
+    """Creating a class Session authentication
     """
     def __init__(self):
         self.user_id_by_session_id = {}
@@ -26,3 +27,12 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+    def current_user(self, request=None) -> str:
+        """Return a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        if session_id:
+            user_id = self.user_id_for_session_id(session_id)
+            if user_id:
+                return User.get(user_id)
+        return None
