b82a2b94f3b7cc8861724e3fd7ede62cc51864d1
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 05:05:37 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Basic API authentication.
 """
-from typing import Tuple, TypeVar 
+from typing import Tuple, TypeVar
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """ Return Empity for now
     """
-    pass 
+    pass
