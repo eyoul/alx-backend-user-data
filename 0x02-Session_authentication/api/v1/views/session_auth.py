d88309a911439f16e113bbee808003466e09e9ba
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 16:31:57 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -32,3 +32,17 @@ def auth_session():
     session_name = os.getenv('SESSION_NAME')
     resp.set_cookie(session_name, session_id)
     return resp
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """ DELETE /auth_session/logout
+    Return:
+      - Empty JSON
+      - Deletes the user session / logout
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    return jsonify({}), 200
