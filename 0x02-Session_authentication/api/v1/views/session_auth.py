49b0484f92a51270e6df084c47d3ca97d4772190
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 12:28:47 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,7 +5,6 @@ import os
 from flask import jsonify, request
 from api.v1.views import app_views
 from models.user import User
-from api.v1.app import auth
 
 
 @app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
@@ -14,18 +13,18 @@ def login() -> str:
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not email:
+    if email is None or email == '':
         return jsonify({'error': 'email missing'}), 400
-    if not password:
+    if password is None or password == '':
         return jsonify({'error': 'password missing'}), 400
 
     user = User.search({'email': email})
-    if not user:
+    if len(user) == 0:
         return jsonify({'error': 'no user found for this email'}), 404
-
+    user = user[0]
     if not user.is_valid_password(password):
         return jsonify({'error': 'wrong password'}), 401
-
+    from api.v1.app import auth
     session_id = auth.create_session(user.id)
     user_dict = user.to_json()
     response = jsonify(user_dict)
