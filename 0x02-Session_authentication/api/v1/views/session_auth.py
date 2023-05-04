3c7ed135646cb1cffccdc6b990f53d4d317c6a9a
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 04:34:40 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++
@@ -1,8 +0,0 @@
-import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.session_auth import SessionAuth
-
-if os.environ.get('AUTH_TYPE') == 'session_auth':
-    auth = SessionAuth()
-else:
-    auth = Auth()
