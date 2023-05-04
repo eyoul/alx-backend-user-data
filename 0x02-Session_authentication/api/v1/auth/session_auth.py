b085bf375cdf5b937a1df48f356a0e54262df636
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 10:35:51 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,26 +10,25 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """Creating a class attribute
+    """Session authentication
     """
-    def __init__(self):
-        self.user_id_by_session_id = {}
-
-    def create_session(self, user_id: str) -> str:
-        """Creating an session instance method
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user ID
         """
+        if user_id is None or not isinstance(user_id, str):
+            return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Creating an user_id_for_session_id method
+        """ Return a User ID based on a Session ID
         """
         if session_id is None or not isinstance(session_id, str):
             return None
-        return self.user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id, None)
 
-    def current_user(self, request=None) -> User:
+    def current_user(self, request: request = None) -> TypeVar('User'):
         """ Return a User instance based on a cookie value
         """
         session_id = self.session_cookie(request)
