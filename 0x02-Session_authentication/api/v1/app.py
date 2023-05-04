49617658ef28919565c015d5e02de14960e44350
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 01:07:13 -0700

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,13 +35,14 @@ def before_request() -> str:
     ]
     if auth.require_auth(request.path, excluded_paths):
         auth_header = auth.authorization_header(request)
-        request.current_user = auth.current_user(request)
-        user = request.current_user
+        user = auth.current_user(request)
         if auth_header is None:
             abort(401)
         if user is None:
             abort(403)
+    # Assign result of auth.current_user(request) to request.current_user
 
+    request.current_user = user
 
 @app.errorhandler(404)
 def not_found(error) -> str:
