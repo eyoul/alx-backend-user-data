521481701a2286d6b34040beeace555fca770cb3
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 10:41:05 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,30 +10,29 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session authentication
+    """Creating a class attribute
     """
-    def create_session(self, user_id: str = None) -> str:
-        """Create a Session ID for a user ID
+    def __init__(self):
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str) -> str:
+        """Creating an session instance method
         """
-        if user_id is None or not isinstance(user_id, str):
-            return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Return a User ID based on a Session ID
+        """Creating an user_id_for_session_id method
         """
         if session_id is None or not isinstance(session_id, str):
             return None
-        return self.user_id_by_session_id.get(session_id, None)
+        return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> User:
         """ Return a User instance based on a cookie value
         """
         session_id = self.session_cookie(request)
-        if session_id:
-            user_id = self.user_id_for_session_id(session_id)
-            if user_id:
-                return User.get(user_id)
-        return None
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
+
