6f0f2bf7793db64744818829353881ec4db76457
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 05:17:00 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 """Basic API authentication.
 """
+import os
+import uuid
 from typing import Tuple, TypeVar
 from .auth import Auth
 
 
 class SessionAuth(Auth):
-    """ Return Empity for now
-    """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.__class__.user_id_by_session_id[session_id] = user_id
+        return session_id
