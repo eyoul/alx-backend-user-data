10fab03037b33df391da2d0b50d8505f25055da2
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 15:53:23 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-import os
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models.user import User
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def auth_session():
-    """
-    Handle user login
-    Return:
-        dictionary representation of user if found else error message
+@app_views.route('/auth_session/login', methods=['POST', 'OPTIONS'], strict_slashes=False)
+@app_views.route('/auth_session/login/', methods=['POST', 'OPTIONS'], strict_slashes=False)
+def auth_session_login():
+    """POST /api/v1/auth_session/login
     """
     email = request.form.get('email')
     password = request.form.get('password')
+
     if email is None or email == '':
         return jsonify({ "error": "email missing" }), 400
     if password is None or password == '':
         return jsonify({ "error": "password missing" }), 400
-    users = User.search({ "email": email })
-    if not users or users == []:
-        return jsonify({ "error": "no user found for this email" }), 404
-    for user in users:
-        if user.is_valid_password(password):
-            from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            resp = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
-            resp.set_cookie(session_name, session_id)
-            return resp
-    return jsonify({ "error": "wrong password" }), 401
+
+    user_list = User.search({'email': email})
+    if len(user_list):
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = user_list[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(auth.session_cookie_name, session_id)
+    return response, 200
