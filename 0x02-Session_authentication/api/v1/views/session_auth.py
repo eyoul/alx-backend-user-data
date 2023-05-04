e9284834fc10a62e849f31b44c992de233803825
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 13:06:59 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,26 +8,29 @@ from flask import abort, jsonify, request
 from models.user import User
 
 
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+from api.v1.app import auth
+
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> str:
-    """Create a new session for a user
-    """
+@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+def auth_session_login() -> str:
+    """Authenticates a user using session authentication"""
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == '':
-        return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
-        return jsonify({"error": "password missing"}), 400
-    print(email, password)
+    if not email:
+        return jsonify({'error': 'email missing'}), 400
+    if not password:
+        return jsonify({'error': 'password missing'}), 400
     user = User.search({'email': email})
-    if len(user) == 0:
-        return jsonify({"error": "no user found for this email"}), 404
-    user = user[0]
+    if not user:
+        return jsonify({'error': 'no user found for this email'}), 404
     if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
+        return jsonify({'error': 'wrong password'}), 401
     session_id = auth.create_session(user.id)
-    cookie_name = getenv('SESSION_NAME')
-    response = jsonify(user.to_json())
-    response.set_cookie(cookie_name, session_id)
+    user_dict = user.to_json()
+    response = jsonify(user_dict)
+    response.set_cookie(os.getenv('SESSION_NAME'), session_id)
     return response
