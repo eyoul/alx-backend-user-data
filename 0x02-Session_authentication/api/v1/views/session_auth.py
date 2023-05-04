7ae0cf2155297333d195bff6630a96a717672f9c
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 15:57:09 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
+from os import getenv
+
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models.user import User
 
 
 @app_views.route('/auth_session/login', methods=['POST', 'OPTIONS'], strict_slashes=False)
-@app_views.route('/auth_session/login/', methods=['POST', 'OPTIONS'], strict_slashes=False)
 def auth_session_login():
     """POST /api/v1/auth_session/login
     """
@@ -21,13 +22,14 @@ def auth_session_login():
 
     user_list = User.search({'email': email})
     if len(user_list):
-        return jsonify({"error": "no user found for this email"}), 404
+        return jsonify({ "error": "no user found for this email" }), 404
 
     user = user_list[0]
     if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+        return jsonify({ "error": "wrong password" }), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
+    cookie_name = getenv('SESSION_NAME')
     response = jsonify(user.to_json())
-    response.set_cookie(auth.session_cookie_name, session_id)
+    response.set_cookie(cookie_name, session_id)
     return response, 200
