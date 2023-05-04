a0b2552a8e4c6dd9ea68a736a84e2a46e24f2fab
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 11:59:27 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 import os
-from flask import jsonify, request
 from api.v1.views import app_views
-from models.user import User
 from api.v1.app import auth
+from flask import abort, jsonify, request
+from models.user import User
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
-def create_session() -> str:
-    """Create a new session for a user"""
+def login_session():
+    """Handle session-based authentication login
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
     if not email:
-        return jsonify({'error': 'email missing'}), 400
+        abort(400, {'error': 'email missing'})
     if not password:
-        return jsonify({'error': 'password missing'}), 400
+        abort(400, {'error': 'password missing'})
 
     user = User.search({'email': email})
     if not user:
-        return jsonify({'error': 'no user found for this email'}), 404
+        abort(404, {'error': 'no user found for this email'})
 
     if not user.is_valid_password(password):
-        return jsonify({'error': 'wrong password'}), 401
+        abort(401, {'error': 'wrong password'})
 
     session_id = auth.create_session(user.id)
     user_dict = user.to_json()
