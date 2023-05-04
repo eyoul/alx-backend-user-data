de9c1d6357b66683c7568c558fc2dd301c70fcbe
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 16:29:05 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -21,14 +21,14 @@ def auth_session():
     if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
     users = User.search({"email": email})
-    if not users or users == []:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
-    for user in users:
-        if user.is_valid_password(password):
-            from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            resp = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
-            resp.set_cookie(session_name, session_id)
-            return resp
-    return jsonify({"error": "wrong password"}), 401
+    user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    resp = jsonify(user.to_json())
+    session_name = os.getenv('SESSION_NAME')
+    resp.set_cookie(session_name, session_id)
+    return resp
