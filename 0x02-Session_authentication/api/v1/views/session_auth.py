8f74f963f553d96f46ea0236cf147f06e3592010
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-04 11:55:43 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
-""" Module of Session auth views
-"""
 import os
+from flask import jsonify, request
 from api.v1.views import app_views
+from models.user import User
 from api.v1.app import auth
-from typing import Tuple
-from flask import abort, jsonify, request
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> Tuple[str, int]:
-    """Handle session-based authentication login
-    """
-    not_found_res = {"error": "no user found for this email"}
+@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+def create_session() -> str:
+    """Create a new session for a user"""
     email = request.form.get('email')
-    if email is None or len(email.strip()) == 0:
-        return jsonify({"error": "email missing"}), 400
     password = request.form.get('password')
-    if password is None or len(password.strip()) == 0:
-        return jsonify({"error": "password missing"}), 400
-    try:
-        users = User.search({'email': email})
-    except Exception:
-        return jsonify(not_found_res), 404
-    if len(users) <= 0:
-        return jsonify(not_found_res), 404
-    if users[0].is_valid_password(password):
-        from api.v1.app import auth
-        sessiond_id = auth.create_session(getattr(users[0], 'id'))
-        res = jsonify(users[0].to_json())
-        res.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
-        return res
-    return jsonify({"error": "wrong password"}), 401
+
+    if not email:
+        return jsonify({'error': 'email missing'}), 400
+    if not password:
+        return jsonify({'error': 'password missing'}), 400
+
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({'error': 'no user found for this email'}), 404
+
+    if not user.is_valid_password(password):
+        return jsonify({'error': 'wrong password'}), 401
+
+    session_id = auth.create_session(user.id)
+    user_dict = user.to_json()
+    response = jsonify(user_dict)
+    response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+
+    return response
