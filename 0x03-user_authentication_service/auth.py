7d83edc7d5acbadf3b9ab22c5a5b56774bbb99b0
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-09 15:50:05 -0700

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,17 +3,8 @@
 """
 from db import DB
 from user import User
-from sqlalchemy.orm.exc import NoResultFound
-
 import bcrypt
-
-
-def _hash_password(password: str) -> bytes:
-    """Hashes the given password using bcrypt
-    """
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
@@ -28,8 +19,17 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-            return user
-        raise ValueError(f"User {email} already exists")
+            pass
+
+        hashed_password = self._hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes the given password using bcrypt
+        """
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode(), salt)
+        return hashed
