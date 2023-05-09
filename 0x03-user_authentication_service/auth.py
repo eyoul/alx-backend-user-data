e6b2154791d8ed042f701397ae35566547409294
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-09 12:23:59 -0700

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,21 +1,11 @@
-from flask import Flask
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+id = Column(Integer, primary_key=True)
+email = Column(String(250), nullable=False)
+hashed_password = Column(String(250), nullable=False)
+session_id = Column(String(250), nullable=True)
+reset_token = Column(String(250), nullable=True)
 
-Base = declarative_base()
-
-
-class User(Base):
-    """User model database table
-    """
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
-
-    def __rep__(self):
-        return "<User(email='%s', hashed_password='%s', session_id='%s', reset_token'%s')>" % (
-            self.email, self.hashed_password, self.session_id, self.reset_token)
+def __rep__(self):
+    return "<User(email='%s', hashed_password='%s', \
+        session_id='%s', reset_token'%s')>" % (
+        self.email, self.hashed_password,
+        self.session_id, self.reset_token)
