f276260608b72624645bb051a25e9e29552c973e
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-09 15:01:11 -0700

    Update auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes the given password using bcrypt
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
