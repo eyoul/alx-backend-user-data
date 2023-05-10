b13423c8400d078c3bbb3f91107d7895bcdbed22
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-10 14:31:54 -0700

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,27 +8,30 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def welcome() -> str:
-    """GET welcome page
+@app.route("/", methods=["GET"], strict_slashes=False)
+def Index() -> str:
     """
-    return jsonify(message="Bienvenue")
+    Return json of
+    {"message": "Bienvenue"}
+    """
+    return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'], strict_slashes=False)
-def register_user() -> str:
-    """ POST /users
-    Return:
-        - registered user emails
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
     """
+    Registration of users
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
     try:
-        email = request.form['email']
-        password = request.form['password']
-        user = AUTH.register_user(email=email, password=password)
-        return jsonify(email=user.email, message='user created'), 200
+        user = AUTH.register_user(email, password)
     except ValueError:
-        return jsonify(message='email already registered'), 400
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+    
