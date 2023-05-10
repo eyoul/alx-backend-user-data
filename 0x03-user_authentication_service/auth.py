767db3f26c9f8c3797c26d655848cf4c4d0cc6c1
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-10 11:21:06 -0700

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """return Hash password
 """
+import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
-import bcrypt
 
 def _hash_password(password: str) -> bytes:
     """Hashes the given password using bcrypt
@@ -22,11 +22,13 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user with the given email and password
-        raises:
-            - ValueError if user already exists
         """
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
+            pass
+
+        hashed_password = self._hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
