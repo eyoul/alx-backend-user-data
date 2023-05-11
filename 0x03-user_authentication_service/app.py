20520528da66585482ad3b364645edad126dbbca
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-11 01:51:14 -0700

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,10 +30,10 @@ def users() -> str:
     email = request.form.get('email')
     password = request.form.get('password')
     try:
-        AUTH.register_user(email, password)
-        return jsonify({'email': email, 'message': 'user created'})
+        user = AUTH.register_user(email=email, password=password)
+        return jsonify(email=user.email, message='user created')
     except ValueError:
-        return jsonify({'message': 'email already registered'}), 400
+        return jsonify(message='email already registered'), 400
 
 
 if __name__ == "__main__":
