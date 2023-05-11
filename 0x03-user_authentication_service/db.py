44f54a4366b0fe573048cb15dd4d044d3f24f8ae
eyoul <49184150+eyoul@users.noreply.github.com>
2023-05-11 20:44:12 +0300

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,17 +61,3 @@ class DB:
             else:
                 raise ValueError()
         self._session.commit()
-
-    def get_user_from_session_id(self, session_id):
-        """
-        Find user by session ID
-            Return: the corresponding User or None
-        """
-        user = None
-        if session_id is None:
-            return None
-        try:
-            user = self._db.find_user_by(session_id=session_id)
-        except NoResultFound:
-            return None
-        return user
