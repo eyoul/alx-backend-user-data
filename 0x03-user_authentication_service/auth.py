9dbed442f65eb3b98b8fd01cf88fe4e3dae9db28
eyoul <49184150+eyoul@users.noreply.github.com>
2023-05-11 20:44:06 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,16 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+    def get_user_from_session_id(self, session_id):
+        """
+        Find user by session ID
+            Return: the corresponding User or None
+        """
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
