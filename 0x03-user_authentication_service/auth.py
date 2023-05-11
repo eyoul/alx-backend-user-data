7b38344f1b537563ee8a858f9a61a47a1aa2e511
eyoul <49184150+eyoul@users.noreply.github.com>
2023-05-11 19:43:26 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,17 +38,14 @@ class Auth:
         Args:
             email (str): new user's email address
             password (str): new user's password
-        Return:
-            if no user with given email exists, return newly created user
-            else raise ValueError
         """
         try:
             self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed = _hash_password(password)
             usr = self._db.add_user(email, hashed)
             return usr
-        raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
         """
