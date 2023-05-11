5b08e80cd2afb031f25b222c4466f3bf702d0aa2
eyoul <49184150+eyoul@users.noreply.github.com>
2023-05-11 18:33:21 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,11 +72,13 @@ class Auth:
         Corresponding to the email, generate a
         new UUID and store it in the database
         """
+        user = None
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return None
-
+        if user is None:
+            return None
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
