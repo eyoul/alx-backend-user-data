ab13255f4ad75f58a15de2fbd60d53a98c0fe645
eyoul <49184150+eyoul@users.noreply.github.com>
2023-05-11 20:52:23 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,3 +94,12 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy session akes a single user_id integer argument.
+            Return: and returns None
+        """
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
