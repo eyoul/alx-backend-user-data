90612b4199322bcd448900ebd2f91f036f5760c9
eyoul <49184150+eyoul@users.noreply.github.com>
2023-05-11 20:04:37 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,11 +20,11 @@ def index() -> str:
     Return json response
     {"message": "Bienvenue"}
     """
-    return jsonify(message='Bienvenue')
+    return jsonify({'message': 'Bienvenue'})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register_user() -> str:
+def users() -> str:
     """
     Args:
         email (str): new user's email address
@@ -32,13 +32,13 @@ def register_user() -> str:
     Return:
         Register new users
     """
+    email = request.form.get('email')
+    password = request.form.get('password')
     try:
-        email = request.form['email']
-        password = request.form['password']
-        user = AUTH.register_user(email=email, password=password)
-        return jsonify(email=user.email, message='user created'), 200
+        AUTH.register_user(email, password)
+        return jsonify({'email': email, 'message': 'user created'})
     except ValueError:
-        return jsonify(message='email already registered'), 400
+        return jsonify({'message': 'email already registered'}), 400
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
@@ -50,18 +50,16 @@ def login() -> str:
     Return:
         LogIn users
     """
-    try:
-        email = request.form['email']
-        password = request.form['password']
-        if not AUTH.valid_login(email=email, password=password):
-            session_id = AUTH.create_session(email=email)
-            response = jsonify(email=email, message='logged in')
-            response.set_cookie('session_id', session_id)
-            return response
-        else:
-            abort(401)
-    except Exception:
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
         abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': f'{email}', 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
