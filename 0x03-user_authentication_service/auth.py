b15f29344d6e8e445ae0ab789a681d9e47c63f83
eyoul <49184150+eyoul@users.noreply.github.com>
2023-05-11 20:47:24 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,7 +86,11 @@ class Auth:
         Find user by session ID
             Return: the corresponding User or None
         """
+        user = None
         if session_id is None:
             return None
-        user = self._db.query(User).filter_by(session_id=session_id).first()
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
         return user
